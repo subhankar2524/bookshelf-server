49a405ae8844e49ab7c969be33693f88812159e6
Subhankar Ghosh <subhankarg2524@gmail.com>
2026-03-23 05:55:39 +0530

    ...

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
 app.use("/api/auth", authRoutes);
 app.use("/api/books", bookRoutes);
 app.use("/api/user-actions", userActionsRoutes);
